22faaf49670ff2c14da6716585e564c822d42358
liwei <425528389@qq.com>
2019-07-02 15:34:52 +0800

    genggai

--- js/plugin/service.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$("body").Sonline({
-		Position:"right",//left或right
-		Top:478,//顶部距离，默认200px
-		Effect:true, //滚动或者固定两种方式，布尔值：true或false
-		DefaultsOpen:false, //默认展开：true,默认收缩：false
-		Qqlist:"488581539|李斯,425528389|王翦" //多个QQ用','隔开，QQ和客服名用'|'隔开
-	});
-})
